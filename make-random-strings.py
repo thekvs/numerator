aa6f53b05196bfae8bbd66f26979cc905766a09c
Konstantin Sorokin <kvs@sigterm.ru>
2013-09-03 21:49:23 +0400

    generate random strings of variable length

--- make-random-strings.py
+++ make-random-strings.py
@@ -6,11 +6,18 @@ import string
 
 
 def parse_args():
+
+    def positive_int(string):
+        value = int(string)
+        if value < 1:
+            raise argparse.ArgumentTypeError("value must be a positive integer")
+        return value
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--count", type=int, metavar="arg", required=True,
                         help="number of random strings to generate")
-    parser.add_argument("--length", type=int, metavar="arg", default=32,
-                        help="length of random string")
+    parser.add_argument("--length", type=positive_int, metavar="arg", nargs=2, default=[32, 32],
+                        help="lower and upper limits of random string's length")
     args = parser.parse_args()
 
     return args
@@ -21,7 +28,8 @@ def main():
     characters = string.ascii_uppercase + string.ascii_lowercase + string.digits
 
     for i in xrange(args.count):
-        print ''.join(random.choice(characters) for x in range(args.length))
+        length = random.randint(args.length[0], args.length[1])
+        print ''.join(random.choice(characters) for x in range(length))
 
 
 if __name__ == "__main__":
